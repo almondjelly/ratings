5a26d042ec93cea1f538acd84a9fa64fa9aa7e86
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-21 17:45:52 -0700

    Fixed movies pages, fixed login issues, allowed update ratings

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from jinja2 import StrictUndefined
 from flask import (Flask, render_template, redirect, request,
-                   flash, session)
+                   flash, session, jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, User, Ratings, Movie
 import pdb
@@ -22,13 +22,84 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    if request.args.get("logout") == 'logout':
-        del session['user_id']
-        flash("You've logged out :(")
+    try:
+        if request.args.get("logout") == 'logout':
+            del session['user_id']
+            flash("You've logged out :(")
+            return redirect('/')
+
+        else:
+            return render_template("homepage.html")
+    except:
+        flash("You haven't logged in yet, dummy!")
         return redirect('/')
 
+@app.route('/movies')
+def movie_list():
+    """SHOW ME THOSE MOVIES"""
+
+    movie_list = Movie.query.order_by('title').all()
+    return render_template("movies.html", movie_list=movie_list)
+
+@app.route('/movies/<movie_id>')
+def movie_page(movie_id):
+    """Show details about a specific movie"""
+
+    all_movie_data = db.session.query(Movie.title,
+                                      Movie.release_date,
+                                      Movie.imdb_url,
+                                      Movie.movie_id,
+                                      Ratings.user_id,
+                                      Ratings.score).filter_by(movie_id=movie_id
+                                      ).join(Ratings).all()
+
+    # check if user is logged in
+    try:
+        if session['user_id']:
+            login_yesno = "true"
+    except:
+        login_yesno = "false"
+
+    return render_template("movie_details.html", all_movie_data=all_movie_data, login_yesno=login_yesno)
+
+@app.route('/display_rating')
+def display_rating():
+    """Display that rating, yo!"""
+
+    movie_id = request.args.get("movie_id")
+
+    # queries database (ratings table) for object, filtered on movie and user
+    user_rating = db.session.query(Ratings.score).filter_by(movie_id=movie_id, user_id=session['user_id']).all()
+
+    # jsonified user_rating is passed into displayRateOption()
+    return jsonify(user_rating)
+
+
+@app.route('/add-rating', methods=['POST'])
+def add_rating():
+    """Add or update user rating for a movie."""
+
+    # grabs new_rating & movie_id values from incoming post request sent by addRating
+    # function on the html side
+    new_rating = request.form.get('new_rating')
+    movie_id = request.form.get('movie_id')
+    # queries database (ratings table) for object, filtered on movie and user
+    user_rating = Ratings.query.filter_by(movie_id=movie_id, user_id=session['user_id']).first()
+    if user_rating:
+        # updates user's score with the new rating
+        user_rating.score = new_rating
+
+    # if user has not rated the movie yet, then create a new row in the Ratings
     else:
-        return render_template("homepage.html")
+        new_rating_object = Ratings(user_id=session['user_id'],
+                                    movie_id=movie_id,
+                                    score=new_rating
+                                    )
+        db.session.add(new_rating_object)
+    db.session.commit()
+
+    # new_rating is passed into updateRating()
+    return new_rating
 
 
 @app.route('/users')
@@ -44,9 +115,11 @@ def user_page(user_id):
     """Display information about an individual user."""
 
     user_info = User.query.filter_by(user_id=user_id).first()
-    user_ratings = User.query.filter_by(user_id=user_id).all()
+    user_ratings = db.session.query(Ratings.score,
+                                    Movie.title,
+                                    Movie.imdb_url).filter_by(user_id=user_id
+                                    ).join(Movie).order_by('title').all()
 
-    # UPDATE JINJA FOR LOOP TO POPULATE RATINGS + MOVIE TITLES :) :) YOU CAN DO IT!!!!
     return render_template("user_page.html", user_info=user_info, user_ratings=
         user_ratings)
 
